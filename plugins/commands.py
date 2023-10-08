66ae5fa744b7aacb57bee489c25e6a282b4b56c3
podimonv3 <145585918+podimonv3@users.noreply.github.com>
2023-10-08 14:20:40 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -95,9 +95,8 @@ async def start(client, message):
         InlineKeyboardButton('⚙ Help ⚙', callback_data="help")
     ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_photo(
-            photo=random.choice(PICS),
-            caption=script.SUR_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+        await message.reply_text(         
+            text=script.SUR_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
