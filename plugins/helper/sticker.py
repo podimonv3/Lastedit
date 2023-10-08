fdc5d7a2d11b03b6455d51bb84dd23b3cb648d25
podimonv3 <145585918+podimonv3@users.noreply.github.com>
2023-10-08 09:55:50 +0530

    Update sticker.py

--- plugins/helper/sticker.py
+++ plugins/helper/sticker.py
@@ -4,7 +4,7 @@
 
 from pyrogram import Client, filters
 
-@Client.on_message(filters.command(["stickerid"]))
+@Client.on_message(filters.command(["sticker"]))
 async def stickerid(bot, message):   
     if message.reply_to_message.sticker:
        await message.reply(f"**Sticker ID is**  \n `{message.reply_to_message.sticker.file_id}` \n \n ** Unique ID is ** \n\n`{message.reply_to_message.sticker.file_unique_id}`", quote=True)
